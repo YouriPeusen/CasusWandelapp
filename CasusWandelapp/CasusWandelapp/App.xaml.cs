a016b4a129bff1eaee6eed86d93022420a670dcc
Youri Peusen <youri-peusen@hotmail.com>
2019-01-19 14:12:29 +0100

    Mainpage

--- CasusWandelapp/CasusWandelapp/App.xaml.cs
+++ CasusWandelapp/CasusWandelapp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace CasusWandelapp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LogInPage();
         }
 
         protected override void OnStart()
---
+++ CasusWandelapp/CasusWandelapp/LogInPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CasusWandelapp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LogInPage : ContentPage
+	{
+		public LogInPage()
+		{
+			InitializeComponent();
+		}
+        
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            //bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            //bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            //if (isEmailEmpty || isPasswordEmpty)
+            //{
+            //    loginLabel.Text = "Login Failed";
+            //}
+            //else
+            //{
+                Navigation.PushAsync(new MainPage());
+            //}
+        }
+    }
+}
